ef11c6f4fc9badd9823feac523dc90217ac85f8e
PasanSasmika <pasansasmika198@gmail.com>
2025-05-28 21:41:41 +0530

    Project Setup with tailwind css 💥

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View>
+      <Text className='text-7xl bg-blue-700'>index</Text>
+      <View className="w-10 h-10 bg-blue-500" />
+    </View>
+  )
+}
+
+export default index
